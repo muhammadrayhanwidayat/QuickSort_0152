b9a8d0e7901fb83e780c1c2cca24a34e3df659eb
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-28 10:36:47 +0700

    membuat pengondisian if untuk menukar elemen pivot dengan elemen di indeks j dan membuat pemanggilan rekursif untuk mengurutkan sub array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -67,8 +67,14 @@ void q_short(int low, int high) {
 		
 
 	}
-
-
+	if (low < j) {
+		//swap the pivot element with the element at index j
+		swap(low, j);
+	}
+	//recursive call to sort the left sub array
+	q_short(low, j - 1);//step 12
+	//recursive call to sort the right sub array
+	q_short(j + 1, high);//step13
 }
 
 
