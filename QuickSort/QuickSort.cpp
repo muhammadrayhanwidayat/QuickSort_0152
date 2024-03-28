6537dca386d2ca1cf66a94a4fd403775f288f055
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-28 10:27:29 +0700

    membuat perulangan while di dalam prosedur q_short

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -54,6 +54,13 @@ void q_short(int low, int high) {
 			cmp_count++;
 		}
 		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) {//step 7
+			j--;		//step 8
+			cmp_count++;
+		}
+		cmp_count++;
+		
 
 	}
 
