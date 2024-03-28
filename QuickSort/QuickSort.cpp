2b4a78db9130c18ae003233de9f0d1f03c821210
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-28 10:05:00 +0700

    memperbaiki  (";") dan membuat prosedur input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -3,8 +3,19 @@ using namespace std;
 
 int arr[20];
 int cmp_count = 0;	//number of compatasion
-int mov_count = 0	//number of data movements
+int mov_count = 0;	//number of data movements
 int n;
+
+void input() {
+	while (true) {
+
+	}
+}
+
+
+
+
+
 
 int main() {
 
